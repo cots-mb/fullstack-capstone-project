d84a8b4f93f505aede8d6df1b78c2fc8beb4d7d3
cots-mb <otsubo@meta-blank.com>
2024-09-14 07:44:36 +0100

    Update App.js

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -14,9 +14,12 @@ function App() {
           {/* the final code will not pass the products to every page, but each page will call the server API */}
           <Route path="/" element={<MainPage />} />
           <Route path="/app" element={<MainPage />} />
+          <Route path="/app/login" element={<LoginPage/>} />
+          <Route path="/app/register" element={<RegisterPage />} />
         </Routes>
         </>
   );
 }
 
 export default App;
+
